cae00cc985659a1fdd3b8d5b1e3a80bd98a81439
DHRUV KUMAR <dkiphones620@gmail.com>
2017-08-02 19:58:03 +0530

    start

---
+++ js/resumeBuilder.js
@@ -0,0 +1,70 @@
+var work = {
+    jobs: [{
+        employer: "",
+        title: "",
+        location: "",
+        dates: "",
+        description: ""
+    }, {
+        employer: "",
+        title: "",
+        location: "",
+        dates: "",
+        description: ""
+    }]
+};
+var projects = {
+    projects: [{
+        title: "",
+        dates: "",
+        description: "",
+        images: [""]
+    }, {
+
+        title: "",
+        dates: "",
+        description: "",
+        images: [""]
+    }]
+};
+var bio = {
+    name: "",
+    role: "",
+    welcomeMessage: "",
+    biopic: "",
+    contacts: {
+        mobile: "",
+        email: "",
+        github: "",
+        twitter: "",
+        location: ""
+    },
+    skills: [""]
+}
+var education = {
+    schools: [{
+        name: "",
+        location: "",
+        degreedates: "",
+        url: "",
+        majors: [""]
+    }, {
+        name: "",
+        location: "",
+        degreedates: "",
+        url: "",
+        majors: [""]
+    }],
+    onlineCourses: [{
+        title: "",
+        school: "",
+        dates: "",
+        url: ""
+    }, {
+        title: "",
+        school: "",
+        dates: "",
+        url: ""
+    }]
+
+}
