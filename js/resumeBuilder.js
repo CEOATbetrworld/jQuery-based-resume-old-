b64ce585c73345f66b108fbdd8ff6fe0cf0b97e0
DHRUV KUMAR <dkiphones620@gmail.com>
2017-08-05 13:19:21 +0530

    refine

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,29 +15,29 @@ var work = {
 };
 var projects = {
     projects: [{
-        title: "",
-        dates: "",
-        description: "",
-        images: [""]
+        title: "Decide what you want to do",
+        dates: "2017",
+        description: "well its really great ",
+        images: ["https://ceoatbetrworld.github.io/images/googlelens.png"]
     }, {
 
-        title: "",
-        dates: "",
-        description: "",
-        images: [""]
+        title: "Artificial Intelligence and Machine Learning",
+        dates: "future",
+        description: "Create something to Serve to humanity",
+        images: ["https://ceoatbetrworld.github.io/images/googlecar.jpg"]
     }]
 };
 var bio = {
-    name: "",
-    role: "",
-    welcomeMessage: "",
-    biopic: "",
-    contacts: {
-        mobile: "",
-        email: "",
-        github: "",
-        twitter: "",
-        location: ""
+    name: "Dhruv Kumar",
+    role: "Web Developer",
+    welcomeMessage: "Lets Create A betterworld together",
+    biopic: "https://avatars0.githubusercontent.com/u/21344347?v=4&s=460",
+    contact: {
+        mobile: "+91 8946824714",
+        email: "dkiphones620@gmail.com",
+        github: "CEOATbetrworld",
+        twitter: "betrworld",
+        location: "Jaipur"
     },
     skills: ["HTML5","CSS3","JavaScript","Problem Solving"]
 }
@@ -70,17 +70,74 @@ var education = {
 }
 
 
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(HTMLbioPic.replace("%data%",bio.biopic));
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contact.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contact.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contact.github));
+$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contact.twitter));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contact.location));
+$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage))
 if(bio.skills.length > 0){
   $("#header").append(HTMLskillsStart);
    $("#skills").append(HTMLskills.replace("%data%",bio.skills));
 }
+$(document).click(function(loc){
+   var x = loc.pageX ;
+   var y = loc.pageY ;
+   logClicks(x,y);
+});
 
-for(job in work.jobs){
+function inName(name){
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+    return name[0] +" "+ name[1];
+}
+
+$("#main").append(internationalizeButton);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for(var job = 0 ; job <=work.jobs.length ; job++ ){
     $("#workExperience").append(HTMLworkStart);
     var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
     var formattedWork = HTMLworkTitle.replace("%data%",work.jobs[job].title);
     $(".work-entry:last").append(formattedEmployer + formattedWork);
+    $(".work-entry:last").append(HTMLworkDates.replace("%data%",work.jobs[job].dates));
+    $(".work-entry:last").append(HTMLworkLocation.replace("%data%",work.jobs[job].location));
+    $(".work-entry:last").append(HTMLworkDescription.replace("%data%",work.jobs[job].description));
 }
+
 
 
 
